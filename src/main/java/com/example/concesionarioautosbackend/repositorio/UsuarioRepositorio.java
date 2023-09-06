a86eb19451fee511245b515414fd827b995b096f
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-06 14:20:27 -0500

    Actualizacion UsuarioRepositorio

--- src/main/java/com/example/concesionarioautosbackend/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/UsuarioRepositorio.java
@@ -1,7 +1,29 @@
 package com.example.concesionarioautosbackend.repositorio;
 
 import com.example.concesionarioautosbackend.modelos.UsuarioEntidad;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UsuarioRepositorio extends JpaRepository<UsuarioEntidad, Long> {
+
+    boolean existsByIdentificacion(String identificacion);
+
+    UsuarioEntidad findByIdentificacion(String identificacion);
+    boolean existsByCorreo(String correo);
+    boolean existsByTelefono(String telefono);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UsuarioEntidad u WHERE u.identificacion = :identificacion")
+    void deleteByIdentificacion(String identificacion);
+
+
+    @Query("SELECT DISTINCT u, v.auto.placa AS placa_auto " +
+            "FROM UsuarioEntidad u " +
+            "INNER JOIN VentasEntidad v ON u.identificacion = v.usuario.identificacion " +
+            "WHERE v.auto.placa IS NOT NULL")
+    List<Object[]> obtenerUsuariosConAutos();
 }
