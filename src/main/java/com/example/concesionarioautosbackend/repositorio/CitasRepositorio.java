8934bbb4da232c7295f2a614fb5661edfcfbad87
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-06 14:52:07 -0500

    Update CitasRepositorio.java

--- src/main/java/com/example/concesionarioautosbackend/repositorio/CitasRepositorio.java
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/CitasRepositorio.java
@@ -1,7 +1,21 @@
 package com.example.concesionarioautosbackend.repositorio;
 
 import com.example.concesionarioautosbackend.modelos.CitasEntidad;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CitasRepositorio extends JpaRepository<CitasEntidad, Long> {
+    @Query(value = "SELECT * FROM tbl_citas", nativeQuery = true)
+    List<CitasEntidad> findAllCitas();
+
+    CitasEntidad findByIdcitas(Long idcitas);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM tbl_citas WHERE idcitas = ?1", nativeQuery = true)
+    void deleteByIdcitas(Long idcitas);
 }
