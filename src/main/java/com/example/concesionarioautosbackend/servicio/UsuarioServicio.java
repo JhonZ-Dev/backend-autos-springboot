c3c4125bec5a71b0f75e4432358a95def084f462
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-06 14:19:07 -0500

    Actualizacion metodos CRUD

--- src/main/java/com/example/concesionarioautosbackend/servicio/UsuarioServicio.java
+++ src/main/java/com/example/concesionarioautosbackend/servicio/UsuarioServicio.java
@@ -5,6 +5,8 @@ import com.example.concesionarioautosbackend.repositorio.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 
 public class UsuarioServicio {
@@ -19,5 +21,36 @@ public class UsuarioServicio {
     public UsuarioEntidad obtenerUsuarioPorId(Long id_usuario) {
         return usuarioRepositorio.findById(id_usuario).orElse(null);
     }
+    public boolean existsByIdentificacion(String identificacion) {
+        return usuarioRepositorio.existsByIdentificacion(identificacion);
+    }
+
+    public boolean exsiteConCorreo(String correo){
+        return usuarioRepositorio.existsByCorreo(correo);
+    }
+
+    public boolean existeConTelefono(String telefono){
+        return usuarioRepositorio.existsByTelefono(telefono);
+    }
+
+    public UsuarioEntidad obtenerUsuarioPorIdentificacion(String identificacion){
+        return usuarioRepositorio.findByIdentificacion(identificacion);
+    }
+
+    public List<UsuarioEntidad> obtenerTodos(){
+        return usuarioRepositorio.findAll();
+    }
+
+
+
+    public void eliminar(String cedula){
+        usuarioRepositorio.deleteByIdentificacion(cedula);
+    }
+
+
+    public List<Object[]> obtenerUsuariosConAutos(){
+        return usuarioRepositorio.obtenerUsuariosConAutos();
+    }
+
 
 }
