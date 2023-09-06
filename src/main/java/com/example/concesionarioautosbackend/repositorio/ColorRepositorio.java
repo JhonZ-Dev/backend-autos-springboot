ef30636e9dedea77022835068d2fc9f245d507de
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-05 20:21:19 -0500

    Creación Color Repositorio

---
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/ColorRepositorio.java
@@ -0,0 +1,8 @@
+package com.example.concesionarioautosbackend.repositorio;
+
+import com.example.concesionarioautosbackend.modelos.ColorAutoEntidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColorRepositorio extends JpaRepository<ColorAutoEntidad, Long> {
+
+}
