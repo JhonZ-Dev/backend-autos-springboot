9eca5dc9af0cad70767119607ba5fe0ef7df63ec
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-07 22:17:16 -0500

    Update MarcaAutoService.java

--- src/main/java/com/example/concesionarioautosbackend/servicio/MarcaAutoService.java
+++ src/main/java/com/example/concesionarioautosbackend/servicio/MarcaAutoService.java
@@ -1,7 +1,29 @@
 package com.example.concesionarioautosbackend.servicio;
 
+import com.example.concesionarioautosbackend.modelos.MarcasAutosEntidad;
+import com.example.concesionarioautosbackend.repositorio.MarcaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MarcaAutoService {
+    @Autowired
+    public MarcaRepositorio marcaRepositorio;
+
+    public MarcasAutosEntidad guardar(MarcasAutosEntidad marcasAutosEntidad){
+        return marcaRepositorio.save(marcasAutosEntidad);
+    }
+
+    public List<MarcasAutosEntidad> listarMarcas() {
+        return marcaRepositorio.findAll();
+    }
+    public boolean existsByIMarca(String marca) {
+        return marcaRepositorio.existsByMarca(marca);
+    }
+
+    public MarcasAutosEntidad obtenerAutosMarca(String marca){
+        return marcaRepositorio.findByMarca(marca);
+    }
 }
