c56367df9c07681e26be1af97800ce75dcc480b1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-07 22:15:11 -0500

    Actualización ColorRepositorio

--- src/main/java/com/example/concesionarioautosbackend/repositorio/ColorRepositorio.java
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/ColorRepositorio.java
@@ -4,5 +4,8 @@ import com.example.concesionarioautosbackend.modelos.ColorAutoEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ColorRepositorio extends JpaRepository<ColorAutoEntidad, Long> {
+    boolean existsByColor(String color);
+
+    ColorAutoEntidad findByColor(String color);
 
 }
--- src/main/java/com/example/concesionarioautosbackend/servicio/ColorServicio.java
+++ src/main/java/com/example/concesionarioautosbackend/servicio/ColorServicio.java
@@ -1,4 +1,29 @@
 package com.example.concesionarioautosbackend.servicio;
 
+import com.example.concesionarioautosbackend.modelos.ColorAutoEntidad;
+import com.example.concesionarioautosbackend.repositorio.ColorRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ColorServicio {
+    @Autowired
+    public ColorRepositorio colorRepositorio;
+
+    public ColorAutoEntidad guardar(ColorAutoEntidad colorAutoEntidad){
+        return colorRepositorio.save(colorAutoEntidad);
+    }
+
+    public List<ColorAutoEntidad> listarColores() {
+        return colorRepositorio.findAll();
+    }
+    public boolean existsByColor(String color) {
+        return colorRepositorio.existsByColor(color);
+    }
+
+    public ColorAutoEntidad obtenerAutosColor(String color){
+        return colorRepositorio.findByColor(color);
+    }
 }
