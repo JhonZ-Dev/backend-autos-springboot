6ce8977394fb2ddff2541a38c8df3218b240eb66
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-07 22:18:16 -0500

    Update ModeloAutosRepositorio.java

--- src/main/java/com/example/concesionarioautosbackend/repositorio/ModeloAutosRepositorio.java
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/ModeloAutosRepositorio.java
@@ -4,4 +4,8 @@ import com.example.concesionarioautosbackend.modelos.ModelosAutosEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModeloAutosRepositorio extends JpaRepository<ModelosAutosEntidad, Long> {
+
+    boolean existsByModelo(String modelo);
+
+    ModelosAutosEntidad findByModelo(String modelo);
 }
