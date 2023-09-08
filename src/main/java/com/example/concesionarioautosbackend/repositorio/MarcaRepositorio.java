0cb5968d407a486a15773df7a3a9d6fa5727f844
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-07 22:16:33 -0500

    Update MarcaRepositorio.java

--- src/main/java/com/example/concesionarioautosbackend/repositorio/MarcaRepositorio.java
+++ src/main/java/com/example/concesionarioautosbackend/repositorio/MarcaRepositorio.java
@@ -4,4 +4,7 @@ import com.example.concesionarioautosbackend.modelos.MarcasAutosEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarcaRepositorio extends JpaRepository<MarcasAutosEntidad, Long> {
+    boolean existsByMarca(String marca);
+
+    MarcasAutosEntidad findByMarca(String marca);
 }
