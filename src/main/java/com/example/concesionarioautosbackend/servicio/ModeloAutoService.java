bccd7a90b702e43d57163d57f7248e009756e7e9
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-07 22:18:54 -0500

    Update ModeloAutoService.java

--- src/main/java/com/example/concesionarioautosbackend/servicio/ModeloAutoService.java
+++ src/main/java/com/example/concesionarioautosbackend/servicio/ModeloAutoService.java
@@ -1,7 +1,17 @@
 package com.example.concesionarioautosbackend.servicio;
 
+import com.example.concesionarioautosbackend.modelos.ModelosAutosEntidad;
+import com.example.concesionarioautosbackend.repositorio.ModeloAutosRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ModeloAutoService {
+
+    @Autowired
+    public ModeloAutosRepositorio modeloAutosRepositorio;
+
+    public ModelosAutosEntidad guardar(ModelosAutosEntidad modelosAutosEntidad){
+        return modeloAutosRepositorio.save(modelosAutosEntidad);
+    }
 }
