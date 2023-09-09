142c651b173605dfcd7b6b93236d0a535b4ce2f7
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-08 21:17:13 -0500

    Creación método guardar un usuario

--- src/main/java/com/example/concesionarioautosbackend/controlador/UsuarioControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/UsuarioControlador.java
@@ -1,12 +1,12 @@
 package com.example.concesionarioautosbackend.controlador;
 
+import com.example.concesionarioautosbackend.modelos.UsuarioEntidad;
 import com.example.concesionarioautosbackend.servicio.UsuarioServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -19,7 +19,30 @@ public class UsuarioControlador {
     //llamar al servicio con la anotacion @Autowire
     @Autowired
     private UsuarioServicio usuarioServicio;
+
+    //Generar un numero alearotrio, ya que no estamos relacionando con ID pero es ncesario un ID
+    private Long generarNumeroAleatorio() {
+        // Lógica para generar el número aleatorio aquí
+        return (long) (Math.random() * 100000);
+    }
     //metodo para guardar un usuario
+    @PostMapping("/guardar")
+    public ResponseEntity<String> saveUser(@RequestBody UsuarioEntidad usuarioEntidad) {
+        String identificacion = usuarioEntidad.getIdentificacion();
+
+        // Verificar si ya existe un usuario con la misma identificación
+        if (usuarioServicio.existsByIdentificacion(identificacion)) {
+            logger.warn("Intento de guardar un usuario con identificación duplicada: {}", identificacion);
+            return ResponseEntity.badRequest().body("Identificación duplicada");
+        }
+
+        // Si la identificación no está duplicada, proceder con el guardado
+        usuarioEntidad.setId_usuario(generarNumeroAleatorio());
+        usuarioServicio.saveUser(usuarioEntidad);
+        logger.info("Usuario guardado exitosamente con identificación: {}", identificacion);
+        return ResponseEntity.ok("{\"message\": \"Usuario Guardado Exitosamente.\"}");
+    }
+
 
 
 }
