03f30efe2ad06dbbe079744a3d99d00520b65349
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-08 21:15:04 -0500

    Update UsuarioControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/UsuarioControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/UsuarioControlador.java
@@ -1,5 +1,7 @@
 package com.example.concesionarioautosbackend.controlador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/usuarios")
 public class UsuarioControlador {
+
+    //Activar los logs:_
+    private final Logger logger = LoggerFactory.getLogger(UsuarioControlador.class);
+
+    
+
 }
