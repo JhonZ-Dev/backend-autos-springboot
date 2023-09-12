b0c63bd0a66cd61a5c41d7d37851a4470250ada1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-11 22:32:27 -0500

    Update AutoControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
@@ -1,6 +1,8 @@
 package com.example.concesionarioautosbackend.controlador;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/autos")
-public class AutoController {
+public class AutoControlador {
+
+    private final Logger logger = LoggerFactory.getLogger(AutoControlador.class);
+
 }
