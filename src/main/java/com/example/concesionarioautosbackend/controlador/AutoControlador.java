ec918163e0a38d03ae2a01f71dced1170acb52df
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-11 22:32:58 -0500

    Update AutoControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
@@ -1,8 +1,13 @@
 package com.example.concesionarioautosbackend.controlador;
 
 
+import com.example.concesionarioautosbackend.servicio.AutoServicio;
+import com.example.concesionarioautosbackend.servicio.ColorServicio;
+import com.example.concesionarioautosbackend.servicio.MarcaAutoService;
+import com.example.concesionarioautosbackend.servicio.ModeloAutoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutoControlador {
 
     private final Logger logger = LoggerFactory.getLogger(AutoControlador.class);
+
+    @Autowired
+    public AutoServicio autoServicio;
+
+    @Autowired
+    public ModeloAutoService modeloAutoService;
+    @Autowired
+    public MarcaAutoService marcaAutoService;
+    @Autowired
+    public ColorServicio colorServicio;
 
 }
