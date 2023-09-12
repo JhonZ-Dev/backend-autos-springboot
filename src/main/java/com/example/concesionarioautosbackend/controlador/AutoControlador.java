f7cf53367d6cea9f272a152aafaff4e0b0ac8a22
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-11 22:35:36 -0500

    Update AutoControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/AutoControlador.java
@@ -1,7 +1,7 @@
 package com.example.concesionarioautosbackend.controlador;
 
 
-import com.example.concesionarioautosbackend.modelos.AutoEntidad;
+import com.example.concesionarioautosbackend.modelos.*;
 import com.example.concesionarioautosbackend.servicio.AutoServicio;
 import com.example.concesionarioautosbackend.servicio.ColorServicio;
 import com.example.concesionarioautosbackend.servicio.MarcaAutoService;
@@ -9,6 +9,7 @@ import com.example.concesionarioautosbackend.servicio.ModeloAutoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,6 +48,37 @@ public class AutoControlador {
         logger.info("Auto guardado exitosamente con placa: {}", placa);
         return ResponseEntity.ok("{\"message\": \"Auto Guardado Exitosamente.\"}");
     }
+
+    @PostMapping("/guardarNuevoAuto")
+    public ResponseEntity<String>agregarAutoNuevo(@RequestBody NuevoAutoAgregar nuevoAutoAgregar) {
+        String modelo = nuevoAutoAgregar.getModelo();
+        String anio = nuevoAutoAgregar.getAnio();
+        String color = nuevoAutoAgregar.getColor();
+        String placa = nuevoAutoAgregar.getPlaca();
+        String marca = nuevoAutoAgregar.getMarca();
+        ModelosAutosEntidad modelosAutosEntidad = modeloAutoService.obtenerAutosModelo(modelo);
+        MarcasAutosEntidad marcasAutosEntidad = marcaAutoService.obtenerAutosMarca(marca);
+        ColorAutoEntidad colorAutoEntidad = colorServicio.obtenerAutosColor(color);
+        if (autoServicio.existsByPlaca(placa)) {
+            logger.warn("Intento de guardar un auto con placa duplicada: {}", placa);
+            return ResponseEntity.badRequest().body("Placa duplicada");
+        }
+        if (modelosAutosEntidad == null || marcasAutosEntidad==null || colorAutoEntidad==null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("modelo o marca de auto no encontrado.");
+        }
+        AutoEntidad autoEntidad = new AutoEntidad();
+        autoEntidad.setModelosAutosEntidad(modelosAutosEntidad);
+        autoEntidad.setMarcasAutosEntidad(marcasAutosEntidad);
+        autoEntidad.setColorAutoEntidad(colorAutoEntidad);
+        autoEntidad.setAnio(anio);
+        autoEntidad.setPlaca(placa);
+        autoEntidad.setId_auto(generarNumeroAleatorio());
+        autoServicio.guardar(autoEntidad);
+        logger.info("Auto guardado exitosamente con placa: {}", placa);
+
+        return ResponseEntity.ok("{\"message\": \"Auto agregado correctamente.\"}");
+    }
+
 
 
 
