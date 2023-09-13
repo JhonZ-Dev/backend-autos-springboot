726b4144561cc34dc0037fa44e025e72b6eaea1b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-12 22:49:50 -0500

    Update CitasController.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/CitasController.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/CitasController.java
@@ -1,14 +1,17 @@
 package com.example.concesionarioautosbackend.controlador;
 
+import com.example.concesionarioautosbackend.modelos.CitasEntidad;
+import com.example.concesionarioautosbackend.modelos.NuevaCitaRequest;
+import com.example.concesionarioautosbackend.modelos.UsuarioEntidad;
 import com.example.concesionarioautosbackend.repositorio.CitasRepositorio;
 import com.example.concesionarioautosbackend.servicio.CitasService;
 import com.example.concesionarioautosbackend.servicio.UsuarioServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/citas")
@@ -23,5 +26,24 @@ public class CitasController {
     @Autowired
     public CitasRepositorio citasRepositorio;
 
-
+    @PostMapping("/guardar-cita")
+    public ResponseEntity<String> agregarCita(@RequestBody NuevaCitaRequest request){
+        String identificacion= request.getIdentificacion();
+        String fechaCita = request.getFecha_estimada();
+        String horaEstimada = request.getHora_estimada();
+        String correo= request.getCorreo();
+        String descripcion= request.getDescripcion();
+        UsuarioEntidad usuario = usuarioServicio.obtenerUsuarioPorIdentificacion(identificacion);
+        if (usuario == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario no encontrado.");
+        }
+        CitasEntidad citas = new CitasEntidad();
+        citas.setUsuario(usuario);
+        citas.setFecha_estimada(fechaCita);
+        citas.setHora_estimada(horaEstimada);
+        citas.setCorreo(correo);
+        citas.setDescripcion(descripcion);
+        citasService.generarCita(citas);
+        return ResponseEntity.ok("{\"message\": \"Cita agregada correctamente.\"}");
+    }
 }
