fc8d587cd4ad2ae1a201bb3cdf06a4dc400a14a5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-12 22:45:51 -0500

    Create CitasController.java

---
+++ src/main/java/com/example/concesionarioautosbackend/controlador/CitasController.java
@@ -0,0 +1,11 @@
+package com.example.concesionarioautosbackend.controlador;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/citas")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CitasController {
+}
