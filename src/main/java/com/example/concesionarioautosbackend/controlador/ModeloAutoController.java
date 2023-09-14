73afc8131a0d6d33de6f2dae02c41f0a830fd6ea
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-13 23:20:53 -0500

    Update ModeloAutoController.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
@@ -1,4 +1,11 @@
 package com.example.concesionarioautosbackend.controlador;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/modelo-auto")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ModeloAutoController {
 }
