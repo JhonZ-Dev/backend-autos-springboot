13c79c0a5625a9f83cc1ea17927584e9b574ef41
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-13 23:21:33 -0500

    Update ModeloAutoController.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
@@ -2,9 +2,7 @@ package com.example.concesionarioautosbackend.controlador;
 
 import com.example.concesionarioautosbackend.servicio.ModeloAutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/modelo-auto")
@@ -13,4 +11,9 @@ public class ModeloAutoController {
 
     @Autowired
     ModeloAutoService modeloAutoService;
+
+    @PostMapping("/insertar")
+    public ModelosAutosEntidad guardar(@RequestBody ModelosAutosEntidad modelosAutosEntidad){
+        return modeloAutoService.guardar(modelosAutosEntidad);
+    }
 }
