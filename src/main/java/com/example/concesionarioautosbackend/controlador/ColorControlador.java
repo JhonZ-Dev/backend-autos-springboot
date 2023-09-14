f1bd77ba6d29571dac4b3f6189fd2d9269e1edfc
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-13 23:19:13 -0500

    Update ColorControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/ColorControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/ColorControlador.java
@@ -2,9 +2,7 @@ package com.example.concesionarioautosbackend.controlador;
 
 import com.example.concesionarioautosbackend.servicio.ColorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -13,5 +11,8 @@ public class ColorControlador {
 
     @Autowired
     public ColorServicio colorServicio;
-    
+    @PostMapping("/insertar")
+    public ColorAutoEntidad guardar(@RequestBody ColorAutoEntidad colorAutoEntidad){
+        return colorServicio.guardar(colorAutoEntidad);
+    }
 }
