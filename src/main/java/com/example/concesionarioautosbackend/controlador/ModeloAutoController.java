e74dea667a45ee05ce3ef722c75800ff5d59f340
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-13 23:22:11 -0500

    Update ModeloAutoController.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/ModeloAutoController.java
@@ -3,6 +3,7 @@ package com.example.concesionarioautosbackend.controlador;
 import com.example.concesionarioautosbackend.modelos.ModelosAutosEntidad;
 import com.example.concesionarioautosbackend.servicio.ModeloAutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,12 @@ public class ModeloAutoController {
     @GetMapping("/traer-todos")
     public List<ModelosAutosEntidad> listarModelos() {
         return modeloAutoService.listarModelos();
+    }
+
+    @PostMapping("/guardarModelo")
+    public ResponseEntity<String> saveModelo(@RequestBody ModelosAutosEntidad modelosAutosEntidad) {
+        modelosAutosEntidad.setId_modelos(generarNumeroAleatorio());
+        modeloAutoService.guardar(modelosAutosEntidad);
+        return ResponseEntity.ok("{\"message\": \"Modelo Guardado Exitosamente.\"}");
     }
 }
