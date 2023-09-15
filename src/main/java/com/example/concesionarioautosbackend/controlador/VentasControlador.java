ea2c7cd749d95720974c5b2a53c444b9a72425a2
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-14 22:19:06 -0500

    Update VentasControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -1,5 +1,6 @@
 package com.example.concesionarioautosbackend.controlador;
 
+import com.example.concesionarioautosbackend.servicio.VentasServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,4 +14,5 @@ public class VentasControlador {
 
     @Autowired
     public VentasServicio ventasService;
+    
 }
