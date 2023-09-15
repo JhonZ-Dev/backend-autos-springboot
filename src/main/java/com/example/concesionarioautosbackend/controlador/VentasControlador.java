5850a20273976985998dc544a472fdaa67c6b39b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-14 22:19:52 -0500

    Update VentasControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -4,9 +4,13 @@ import com.example.concesionarioautosbackend.servicio.AutoServicio;
 import com.example.concesionarioautosbackend.servicio.UsuarioServicio;
 import com.example.concesionarioautosbackend.servicio.VentasServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -20,5 +24,11 @@ public class VentasControlador {
     public UsuarioServicio usuarioService;
     @Autowired
     public AutoServicio autoService;
+
+    @GetMapping("/obtenerTodo")
+    public ResponseEntity<List<VentaDTO>> obtenerTodasLasVentas() {
+        List<VentaDTO> ventas = ventasService.obtenerTodasLasVentasConDetalles();
+        return ResponseEntity.ok(ventas);
+    }
 
 }
