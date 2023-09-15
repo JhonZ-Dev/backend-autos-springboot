d3613d4f9079e0ad52a936ee594e2f233bc74adb
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-14 22:21:04 -0500

    Update NuevaVentaRequest.java

--- src/main/java/com/example/concesionarioautosbackend/modelos/NuevaVentaRequest.java
+++ src/main/java/com/example/concesionarioautosbackend/modelos/NuevaVentaRequest.java
@@ -1,7 +1,10 @@
 package com.example.concesionarioautosbackend.modelos;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class NuevaVentaRequest {
 
     private double precio;
