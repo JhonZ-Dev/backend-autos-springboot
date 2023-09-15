8f35eb711bf5832731ecda72e160b79b30aa6cc7
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-14 22:20:17 -0500

    Update VentasControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -5,12 +5,11 @@ import com.example.concesionarioautosbackend.servicio.AutoServicio;
 import com.example.concesionarioautosbackend.servicio.UsuarioServicio;
 import com.example.concesionarioautosbackend.servicio.VentasServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -30,6 +29,32 @@ public class VentasControlador {
     public ResponseEntity<List<VentaDTO>> obtenerTodasLasVentas() {
         List<VentaDTO> ventas = ventasService.obtenerTodasLasVentasConDetalles();
         return ResponseEntity.ok(ventas);
+    }
+
+    @PostMapping("/insertar")
+    public ResponseEntity<String> agregarVenta(@RequestBody NuevaVentaRequest request) {
+        String identificacion=request.getIdentificacion();
+        String placa=request.getPlaca();
+        double precio = request.getPrecio();
+        LocalDate fechaVenta = request.getFecha_venta();
+
+        UsuarioEntidad usuario = usuarioService.obtenerUsuarioPorIdentificacion(identificacion);
+        AutoEntidad auto = autoService.obtenerAutoPorPlaca(placa);
+
+        if (usuario == null || auto == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario o auto no encontrado.");
+        }
+
+        VentasEntidad venta = new VentasEntidad();
+        venta.setUsuario(usuario);
+        venta.setAuto(auto);
+        venta.setPrecio(precio);
+        venta.setFecha_venta(fechaVenta);
+
+        ventasService.guardarVenta(venta);
+
+        return ResponseEntity.ok("{\"message\": \"Venta agregada correctamente.\"}");
+
     }
 
 }
