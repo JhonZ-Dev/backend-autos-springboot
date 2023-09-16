117e34ece5665bafdf644b20eaec4075b5fa3343
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-15 23:41:22 -0500

    commit

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -6,6 +6,7 @@ import com.example.concesionarioautosbackend.servicio.AutoServicio;
 import com.example.concesionarioautosbackend.servicio.UsuarioServicio;
 import com.example.concesionarioautosbackend.servicio.VentasServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
