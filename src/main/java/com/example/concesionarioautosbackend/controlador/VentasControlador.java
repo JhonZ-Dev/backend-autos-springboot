b5d35a3da018e2c97605654475002f0365e0a475
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-15 23:39:00 -0500

    comit

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -1,7 +1,9 @@
 package com.example.concesionarioautosbackend.controlador;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
