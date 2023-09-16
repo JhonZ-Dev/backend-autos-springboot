2d7c67e854e4b5e82c4a72553c8fcdb9c93478a3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-15 23:38:32 -0500

    Update VentasControlador.java

--- src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
+++ src/main/java/com/example/concesionarioautosbackend/controlador/VentasControlador.java
@@ -1,6 +1,7 @@
 package com.example.concesionarioautosbackend.controlador;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
